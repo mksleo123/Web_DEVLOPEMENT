0b2832f07c5f7ab544a59be08a6e20ce27953052
Mayur kumar <jgmmkslksmgwn@gmail.com>
2021-05-03 02:01:09 +0530

    starting with dom

---
+++ DOM/index.js
@@ -0,0 +1,7 @@
+document.querySelector("h1").style.color = "red";
+document.querySelector("h1").style.fontSize = "5rem";
+document.querySelector("button").style.backgroundColor="yellow";
+document.querySelector("button").classList.add("invisible");
+document.querySelector("button").classList.remove("invisible");
+
+document.querySelector("a").setAttribute("href","http://www.bing.com"); 
