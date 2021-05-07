0fe5a40d5dfc05f8be64391c8123a51ec452759e
Mayur kumar <jgmmkslksmgwn@gmail.com>
2021-05-07 14:50:37 +0530

    understanding working of api

---
+++ Newletter-signup/app.js
@@ -0,0 +1,28 @@
+//jshint esversion:6
+
+const express =require("express");
+const bodyParser =require("body-parser");
+const request =require("request");
+
+
+const app= express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req,res){
+  var firstname=req.body.fname;
+  var lastname=req.body.lname;
+  var email =req.body.email;
+
+   console.log(firstname);
+    console.log(lastname);
+     console.log(email);
+
+});
+
+app.listen(3000, function(){
+  console.log("server is running on port 3000");
+});
